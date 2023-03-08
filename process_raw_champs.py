657eb0d9838a475a720a669824507d0ed3a16f4f
imtiyaazgit <imtiyaazgit@gmail.com>
2023-03-08 19:22:18 +0200

    Latest spoken quote dataset as of patch 13.5 excluding the new champion Milio

--- process_raw_champs.py
+++ process_raw_champs.py
@@ -3,7 +3,7 @@ import json
 champs_json = open("processed/champs.jsonl", "w")
 with open("raw/champs.txt", "r") as champs:
     for champ_id, line in enumerate(champs):
-        words = line.split(" ")
+        words = line.split(" ") # Will produce bad results for Lee Sin, Master Yi, Dr. Mundo etc. Manually edit names in processed/champs.jsonl
         name = words[0]
         title = " ".join(words[1:])[0].upper() + " ".join(words[1:])[1:]
         champ_detail = {"id": champ_id, "name": name, "title": title.rstrip("\n")}
--- scrape_champ_quotes.py
+++ scrape_champ_quotes.py
@@ -1,9 +1,13 @@
 import urllib.request as request
 from bs4 import BeautifulSoup as bs
 import json
+import time
 
 endpoint = "https://leagueoflegends.fandom.com/wiki/{}/LoL/Audio"
 champs = "processed/champs.jsonl"
+champ_quotes_file = open("processed/champ_quotes.jsonl", "w")
+error_log = open("raw/parse_errors.txt", "w")
+
 
 def process_champs_jsonl():
     champ_records = []
@@ -18,7 +22,30 @@ def scrape_champ_html(champ_name):
 
 def parse_champ_quotes(html):
     soup = bs(html, "html.parser")
-    quotes = soup.findAll("i", data=True)
-    print(quotes)
+    quotes = soup.findAll("i")
+    return filter(lambda quote: "\"" in quote, map(lambda soup_quote: soup_quote.text, quotes))
 
-parse_champ_quotes(scrape_champ_html(process_champs_jsonl()[0]["name"]))
+def write_champ_quotes(champ_id, champ_name):
+    champ_name = champ_name if len(champ_name.split(" ")) == 1 else "_".join(champ_name.split(" "))
+    parsed_quotes = parse_champ_quotes(scrape_champ_html(champ_name))
+    for qid, quote in enumerate(parsed_quotes):
+        champ_quotes_file.write(json.dumps({"id": qid, "champ_id": champ_id, "quote": quote.lstrip("\"").rstrip("\"").rstrip("\n")}) + "\n")
+
+def write_all_champ_quotes():
+    champ_records = process_champs_jsonl()
+    for champ in champ_records:
+        print("Writing champion quote data for: {}".format(champ["name"]))
+        start = time.time()
+        try:
+            write_champ_quotes(champ["id"], champ["name"])
+            print("\tCOMPLETED - {}s".format(time.time() - start))
+            time.sleep(2)
+        except Exception as e:
+            print("Could not get quotes for: " + champ["name"])
+            print("Logged to raw/parse_errors.txt")
+            error_log.write("Could not parse quotes for: " + champ["name"] + "\n")
+    champ_quotes_file.close()
+    error_log.close()
+        
+
+write_all_champ_quotes()
